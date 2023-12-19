8baf8a141badaaf10753ee3a0502974f690d06c0
Flexus <98521873+FlaviuPalii@users.noreply.github.com>
2023-12-19 09:42:15 +0200

    Add files via upload

--- Fourth.R
+++ Fourth.R
@@ -1,5 +1,5 @@
-# Instalare pachete (dacă nu sunt instalate)
-install.packages(c("tidyverse", "lattice", "rsample", "vip"))
+install.packages(c("tidyverse", "lattice", "rsample", "vip", "skimr"))
+install.packages("GGally")
 
 # Incărcare pachete
 library(tidyverse)
@@ -8,7 +8,9 @@ library(rsample)
 library(vip)
 library(caret)
 library(lmtest)
-
+library(skimr)
+library(GGally)
+library(ggplot2)
 
 # Calea către fișierul CSV
 data <- "ames.csv"
@@ -21,6 +23,12 @@ glimpse(ames)
 
 ames <- ames %>%
   select_if(~ !any(is.na(.)))  # Selectează doar coloanele fără date lipsă
+
+# Vizualizarea datelor într-un format tabular
+View(ames)
+
+# Obținerea unui rezumat statistic detaliat
+skim(ames)
 
 
 # Splitarea setului de date
@@ -64,6 +72,20 @@ cv_model2 <- train(
 # Evaluare performanță modele
 summary(resamples(list(model1 = cv_model1, model2 = cv_model2)))
 
+# Print out the column names of the dataframe
+print(names(ames))
+
+# Corrected selected variables
+selected_variables <- c("Year.Built", "Yr.Sold", "Lot.Area", "Kitchen.Qual", "Overall.Qual", "MS.Zoning", "Heating")
+
+# Create the pair plot with the selected variables
+p <- ggpairs(ames[, selected_variables])
+
+# Display the plot
+print(p)
+
+
+
 # Analiza condițiilor
 # Liniaritatea pentru modelul 1
 plot(model1)
@@ -73,10 +95,6 @@ plot(model1, which = c(1, 3))
 # Normalitatea reziduurilor
 qqPlot(model1$residuals)
 
-# Test pentru independența reziduurilor
-dwtest(model1)
-
-
 # Liniaritatea pentru modelul 2
 plot(model2)
 
@@ -85,13 +103,9 @@ plot(model2, which = c(1, 3))
 # Normalitatea reziduurilor
 qqPlot(model2$residuals)
 
-# Test pentru independența reziduurilor
-dwtest(model2)
 
 # Interpretare caracteristici (pentru modelul 3)
 vip(cv_model2, num_features = 10)
-
-
 
 # Regresie liniară inițială cu Year si LotArea
 single_level_factors <- sapply(ames_train, function(x) is.factor(x) && length(levels(x)) <= 1)
@@ -151,15 +165,45 @@ cat("R-squared:", rsquared_final_model_with_qual, "\n\n")
 predictions_final_model <- predict(final_model, newdata = ames_test)
 predictions_final_model_with_qual <- predict(final_model_with_qual, newdata = ames_test)
 
-# Creăm diagrame de dispersie pentru compararea valorilor reale cu predicțiile
-plot(ames_test$price, predictions_final_model, main = "Predicții vs. Valori reale pentru Modelul Final (Stepwise)",
-     xlab = "Valori reale", ylab = "Predicții", col = "blue")
-abline(0, 1, col = "red")  # Linia de referință pentru valorile reale egale cu predicțiile
+# Calculează diferențele absolute între valorile reale și predicții
+differences1 <- abs(ames_test$price - predictions_final_model)
+differences2 <- abs(ames_test$price - predictions_final_model_with_qual)
+
+# Funcție pentru a converti diferențele într-un gradient de culori, cu mai multă pondere spre roșu
+get_color_gradient <- function(diffs) {
+  max_diff <- max(diffs)
+  num_colors <- 100 # Numărul de culori în gradient
+  color_palette <- colorRampPalette(c("blue", "red"))(num_colors)
+  
+  # Crează un set de praguri pentru intervalul de culori, cu mai multe valori apropiate de roșu
+  thresholds <- c(seq(0, max_diff * 0.2, length.out = num_colors - 10), 
+                  seq(max_diff * 0.2, max_diff, length.out = 10))
+  
+  diff_colors <- color_palette[findInterval(diffs, thresholds)]
+  return(diff_colors)
+}
+
+
+# Plotează primul grafic
+plot(ames_test$price, predictions_final_model, 
+     main = "Predicții vs. Valori reale pentru Modelul Final (Stepwise)",
+     xlab = "Valori reale", ylab = "Predicții", 
+     pch = 20,  # Utilizează un punct solid mic pentru plotare
+     col = get_color_gradient(differences1))
+abline(0, 1, col = "orange", lwd = 3)  # Linie mai groasă pentru linia de predicție
+legend("topleft", legend = "Valori reale = Predicții", col = "red", lty = , cex = 0.8)
+
+# Plotează al doilea grafic
+plot(ames_test$price, predictions_final_model_with_qual, 
+     main = "Predicții vs. Valori reale pentru Modelul Final (Stepwise cu Overall.Qual)",
+     xlab = "Valori reale", ylab = "Predicții",
+     pch = 20,  # Utilizează un punct solid mic pentru plotare
+     col = get_color_gradient(differences2))
+abline(0, 1, col = "orange", lwd = 3)
 legend("topleft", legend = "Valori reale = Predicții", col = "red", lty = 1, cex = 0.8)
 
-plot(ames_test$price, predictions_final_model_with_qual, main = "Predicții vs. Valori reale pentru Modelul Final (Stepwise cu Overall.Qual)",
-     xlab = "Valori reale", ylab = "Predicții", col = "blue")
-abline(0, 1, col = "red")  # Linia de referință pentru valorile reale egale cu predicțiile
-legend("topleft", legend = "Valori reale = Predicții", col = "red", lty = 1, cex = 0.8)
+
+dwtest(model1)
+dwtest(model2)
 
 
